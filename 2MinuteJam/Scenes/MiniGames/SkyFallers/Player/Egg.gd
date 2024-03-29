8269b71d77c2e5d4ef096670016705001c3c808c
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-03-29 12:12:28 -0400

    added game to arcade

--- 2MinuteJam/Scenes/MiniGames/SkyFallers/Player/Egg.gd
+++ 2MinuteJam/Scenes/MiniGames/SkyFallers/Player/Egg.gd
@@ -4,9 +4,9 @@ extends Area2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y -=2
-	$Sprite2D.rotation_degrees += 2
-	$CollisionShape2D.rotation_degrees += 2
+	position.y -=120 * delta
+	$Sprite2D.rotation_degrees += 120* delta
+	$CollisionShape2D.rotation_degrees += 60 * delta
 
 
 
--- 2MinuteJam/Scenes/MiniGames/SkyFallers/SkyFallers.gd
+++ 2MinuteJam/Scenes/MiniGames/SkyFallers/SkyFallers.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var direction = -1
 var time : float = 1
+@onready var chicken = preload("res://2MinuteJam/Scenes/MiniGames/SkyFallers/Enemy/Chicken.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +20,20 @@ func speedUP():
 	time -= .03
 
 
-func _on_area_2d_area_entered(area):
-	direction *= -1
+func _on_area_2d_area_entered(_area):
+	direction = -1
 	get_tree().call_group("enemies", "down")
+
+
+func _on_area_2d_2_area_entered(area):
+	var c = chicken.instantiate()
+	get_parent().call_deferred("add_child", c) 
+	c.global_position = area.global_position
+	area.queue_free()
+	
+
+
+func _on_area_2d_3_area_entered(_area):
+	direction = 1
+	get_tree().call_group("enemies", "down")
+
