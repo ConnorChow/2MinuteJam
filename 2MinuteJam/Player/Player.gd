4ddeb689e910b7545df3d98cb818e216fd494469
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-03-29 17:49:19 -0400

    added sitting to objects marked "sit"

--- 2MinuteJam/Player/Player.gd
+++ 2MinuteJam/Player/Player.gd
@@ -9,6 +9,9 @@ const JUMP_VELOCITY : float = 4.0
 
 var gravity : float = 9.8
 
+var canleave: bool = true
+var sitting : bool = false
+
 var direction
 var headDir
 
@@ -19,6 +22,8 @@ var hitObject :Object
 @onready var camera = $Node3D/Camera3D
 @onready var holdPoint = $Node3D/Camera3D/holdPoint
 @onready var rayCast = $Node3D/Camera3D/colissionRay
+@onready var unsitTimer = $unsitTimer
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
@@ -42,6 +47,9 @@ func _physics_process(delta):
 	headDir = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if Input.is_action_just_pressed("jump") and is_on_floor() && !GlobalController.inDialogue:
 		velocity.y = JUMP_VELOCITY
+		if canleave:
+			sitting = false
+			canleave = false
 	
 	if heldObject:
 		heldObject.global_position = holdPoint.global_position
@@ -106,7 +114,15 @@ func _physics_process(delta):
 			#			heldObject.mode = RigidBody3D.FREEZE_MODE_KINEMATIC
 					if hitObject.is_in_group("interact"):
 						hitObject.interact()
+					if hitObject.is_in_group("sit"):
+						unsitTimer.start()
+						canleave = false
+						sitting = true
+						global_position = hitObject.get_child(0).global_position
 					else:pass
+			if canleave:
+				canleave = false
+				sitting = false
 	if GlobalController.inDialogue:
 		velocity = Vector3(0, -gravity*delta, 0)
 	else:
@@ -126,6 +142,11 @@ func _physics_process(delta):
 
 				
 	velocity.normalized()
-	move_and_slide()
+	if !sitting:
+		move_and_slide()
 
 
+
+
+func _on_unsit_timer_timeout():
+	canleave = true
