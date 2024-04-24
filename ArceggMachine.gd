9d1590922d22f8a104bff3b538dd6d3b497d53a3
ConnorChow <connorjameschow@gmail.com>
2024-04-23 21:40:39 -0400

    Arcade Works?

---
+++ ArceggMachine.gd
@@ -0,0 +1,17 @@
+extends MeshInstance3D
+@onready var viewport = $SubViewport
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	viewport.set_clear_mode(SubViewport.CLEAR_MODE_ONCE)
+	
+	#(material_override as StandardMaterial3D).albedo_texture = viewport.get_texture()
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_texture = viewport.get_texture()
+	set_surface_override_material(1, material)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
